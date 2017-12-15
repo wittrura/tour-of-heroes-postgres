2b5d7e301dff873bc3f30e2c117708f306fde40a
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-12-15 12:35:59 -0600

    Deployment: adds paths for migrations and seed files in production

--- knexfile.js
+++ knexfile.js
@@ -25,6 +25,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 };
